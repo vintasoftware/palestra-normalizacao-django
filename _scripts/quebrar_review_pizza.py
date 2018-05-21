6ff119b55c9d5aae4922829b51328844dd59c2b3
Flávio Juvenal <flaviojuvenal@gmail.com>
2018-05-21 19:12:31 -0300

    Renomear para pizzaproject

--- _scripts/quebrar_review_pizza.py
+++ _scripts/quebrar_review_pizza.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # fix PYTHONPATH
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "normalization.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizzaproject.settings")
 import django
 django.setup()
 
--- pizzaproject/wsgi.py
+++ pizzaproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for normalization project.
+WSGI config for pizzaproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "normalization.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizzaproject.settings")
 
 application = get_wsgi_application()
