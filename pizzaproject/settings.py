f995db1e689db06f16ff20d5fd9bfa039f237744
Flávio Juvenal <flaviojuvenal@gmail.com>
2018-05-22 15:14:14 -0300

    Linguagem pt-br e timezone America/Recife

--- pizzaproject/settings.py
+++ pizzaproject/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = []
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Recife'
 
 USE_I18N = True
 
